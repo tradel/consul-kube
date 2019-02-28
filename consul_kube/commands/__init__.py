5d8276a37644dd269ed72b2b9d5d87e6796c0737
Todd Radel <todd@hashicorp.com>
2019-02-28 15:22:59 -0500

    Added CLI flag for kube context selection

--- consul_kube/commands/__init__.py
+++ consul_kube/commands/__init__.py
@@ -6,6 +6,7 @@ from consul_kube.lib import color
 @click.group()
 @click.option('-debug/-no-debug', default=False, help='Enables or disables verbose output.')
 @click.option('-save-certs/-no-save-certs', default=False, help='Save a copy of any retrieved certs.')
+@click.option('-context', help='Choose a context from your kubeconfig.')
 @click.pass_context
 def main(ctx: click.Context, debug: bool, save_certs: bool):
     ctx.ensure_object(dict)
