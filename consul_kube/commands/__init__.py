e6aa61a8a71de8703faec051d43880fa1463e113
tradel <todd@radel.us>
2019-03-02 10:33:12 -0500

    Move namespace option to global

--- consul_kube/commands/__init__.py
+++ consul_kube/commands/__init__.py
@@ -13,27 +13,31 @@ from consul_kube.commands.validate import validate_command
               help='Save a copy of any retrieved certs.')
 @click.option('-context', 'context_name',
               help='Choose a context from your kubeconfig.')
+@click.option('-namespace', default='default',
+              help='Kubernetes namespace where we can find Consul.')
 @click.version_option(__version__, '-version')
 @click.help_option('-help')
 @click.pass_context
-def main(ctx: click.Context, debug: bool, save_certs: bool, context_name: str) -> None:
+def main(ctx: click.Context, debug: bool, save_certs: bool, context_name: str, namespace: str) -> None:
     ctx.ensure_object(dict)
     ctx.obj['debug'] = debug
     ctx.obj['save_certs'] = save_certs
+    ctx.obj['context_name'] = context_name
+    ctx.obj['namespace'] = namespace
     color.debug_mode = debug
     color.write_certs = save_certs
 
 
 @main.command()
-@click.option('-namespace', default='default', help='Kubernetes namespace where we can find Consul.')
+@click.help_option('-help')
 @click.pass_context
-def validate(ctx: click.Context, namespace: str) -> None:
+def validate(ctx: click.Context) -> None:
     """Checks the certificates for every injected pod."""
-    ctx.obj['namespace'] = namespace
     validate_command(ctx)
 
 
 @main.command()
+@click.help_option('-help')
 @click.pass_context
 def rotate(ctx: click.Context) -> None:
     """Forces the Consul Connect CA to rotate its root certificate."""
